d5a5b355f2010b4743a227e46d7d2234f83aad3e
Iltan Caner <iltancanerr@gmail.com>
2025-12-14 04:27:52 -0500

    Add Prettier configuration and Tailwind CSS plugin support

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  "trailingComma": "es5",
+  "plugins": ["prettier-plugin-tailwindcss"],
+}
+
+export default config;
